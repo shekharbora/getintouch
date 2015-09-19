aa3f79ba993df04f8ce95c6e5e3b128b501fc4a9
shekharbora <you@example.com>
2015-09-19 18:43:28 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823082136) do
+ActiveRecord::Schema.define(version: 20150919114900) do
 
   create_table "blogs", force: true do |t|
     t.string   "title"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20150823082136) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
